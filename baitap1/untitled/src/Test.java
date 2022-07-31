dfa5d8d27d242e5eb5ef48cde39798f1674b8432
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-07-31 11:18:05 +0700

    bai thuc hanh

---
+++ baitap1/untitled/src/Test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<String> stackChuoi = new Stack<String>();
+    }
+}
---
+++ trien khai stack/src/GenericStackClient.java
@@ -0,0 +1,11 @@
+
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        new MyGenericStack.stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        new MyGenericStack.stackOfIStrings();
+    }
+
+
+}
